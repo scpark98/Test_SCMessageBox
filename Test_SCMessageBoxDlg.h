4fb43af7adc034591a13bfbfcce4d041f14625a9
scpark98 <scpark98@gmail.com>
2025-03-28 17:57:07 +0900

    초기 커밋.

---
+++ Test_SCMessageBoxDlg.h
@@ -0,0 +1,39 @@
+﻿
+// Test_SCMessageBoxDlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include "../../Common/CDialog/CSCMessageBox/SCMessageBox.h"
+
+// CTestSCMessageBoxDlg 대화 상자
+class CTestSCMessageBoxDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CTestSCMessageBoxDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+protected:
+	CSCMessageBox	m_message;
+
+public:
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TEST_SCMESSAGEBOX_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
